3c5e64cfba3ba5e64bfaf6bc20ec160e355dcc51
kettrin62 <Kettykazakova@yandex.ru>
2023-02-05 16:12:08 +0000

    fix: server

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const URL = "http://api.kett.kupipodariday.nomoredomainsclub.ru";
+export const URL = "http://kett.kupipodariday.nomoredomainsclub.ru:4000";
 
 export const MINIMUM_PASSWORD_LENGTH = 3;
 export const MINIMUM_USERNAME_LENGTH = 3;
